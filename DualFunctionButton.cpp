acbef7e095a94b84b593c7f5848d0e742703bded
Berran Remzi <berran@abv.bg>
2017-12-12 11:10:24 +0000

    Added source code files

---
+++ DualFunctionButton.cpp
@@ -0,0 +1,50 @@
+#include "DualFunctionButton.h"
+
+DualFunctionButton::DualFunctionButton(int buttonP, long longPressT) {
+  this-> buttonPin = buttonP;
+  this-> longPressTime = longPressT;
+  pinMode(this-> buttonPin, INPUT);
+}
+
+bool DualFunctionButton::longPress() {
+  evaluatePress();
+  if (this->longPressDetected == 1) {
+    this->longPressDetected = 0;
+    return true;
+  }
+  else return 0;
+}
+
+bool DualFunctionButton::shortPress() {
+  evaluatePress();
+  if (this->shortPressDetected == 1) {
+    this->shortPressDetected = 0;
+    return true;
+  }
+  else return 0;
+}
+
+void DualFunctionButton::evaluatePress() {
+  if (digitalRead(this->buttonPin) == HIGH) {
+    if (this->buttonActive == false) {
+      this->buttonActive = true;
+      this->buttonTimer = millis();
+    }
+    if ((unsigned long)(millis() - buttonTimer > longPressTime) && (longPressActive == false)) {
+      this->longPressActive = true;
+      this->longPressDetected = true;
+    }
+  } else {
+    if (this->buttonActive == true) {
+      if (this->longPressActive == true) {
+        this->longPressActive = false;
+      }
+      else {
+        this->shortPressDetected = true;
+      }
+      this->buttonActive = false;
+    }
+  }
+}
+
+
---
+++ DualFunctionButton.h
@@ -0,0 +1,26 @@
+#ifndef DUALFUNCTIONBUTTON_H
+#define DUALFUNCTIONBUTTON_H
+
+#include <Arduino.h>
+
+class DualFunctionButton {
+  public:
+    DualFunctionButton(int buttonP, long longPressT);
+    bool longPress();
+    bool shortPress();
+    void evaluatePress();
+    
+  private:
+    int buttonPin;
+    boolean outputOneState = false;
+    boolean outputTwoState = false;
+    boolean shortPressDetected = false;
+    boolean longPressDetected = false;
+    unsigned long buttonTimer = 0;
+    unsigned int longPressTime = 250;
+
+    boolean buttonActive = false;
+    boolean longPressActive = false;
+};
+
+#endif
